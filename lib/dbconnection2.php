89c0acab5d091476c3f33f7a9c22d9f4b1ca25bb
it134157 <73292440+it134157@users.noreply.github.com>
2020-12-02 17:14:39 +0200

    02 Dec 2020 Update

--- lib/dbconnection2.php
+++ lib/dbconnection2.php
@@ -7,15 +7,16 @@ $user = $DB_USER;
 $pass = $DB_PASS;
 $nopass = '';
 
-if(gethostname() ==  'users.iee.ihu.gr'){
-	$mysqli = new mysqli($host, $user, $pass, $db, null, 'home/students/it134157/mysql/run/mysl.sock');
+if(gethostname() == 'users.iee.ihu.gr') {
+	$mysqli = new mysqli($host, $user, $pass, $db, null, 'home/students/it134157/mysql/run/mysql.sock');
 } else {
-	$mysqli = new mysqli($host, $user, $nopass, $db);
+        $mysqli = new mysqli($host, $user, $nopass, $db);
 }
 
-if($mysqli -> connect_errno){
-	echo "Failed to connect to MySQL: (" .
-	$mysqli -> connect_errno . ") " . $mysqli -> connect_error;
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: (" . 
+    $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
 
 ?>
+
--- lib/game.php
+++ lib/game.php
@@ -1,14 +1,14 @@
 <?php
-function show_status(){
+function show_status() {
+	
 	global $mysqli;
-
 	$sql = 'SELECT * FROM game_status';
-	$st = $mysql -> prepare($sql);
-	
-	$st -> execute();
-	$res = $st -> get_result();
-	
+	$st = $mysqli->prepare($sql);
+
+	$st->execute();
+	$res = $st->get_result();
+
 	header('Content-type: application/json');
-	print json_encode($res -> fetch_all(MYSQL_ASSOC), JSON_PRETTY_PRINT);
+	print json_encode($res->fetch_all(MYSQLI_ASSOC), JSON_PRETTY_PRINT);
 }
 ?>

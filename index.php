e446b606d53f8afb7ee3eea0c50afed3d4f6e3d6
it134157 <73292440+it134157@users.noreply.github.com>
2020-12-08 17:13:36 +0200

    Update index.php

--- index.php
+++ index.php
@@ -5,8 +5,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <title>ΑΔΙΣΕ: ΤΑΒΛΙ</title>
+    <title>ADISE20_134157</title>
+	
+	<link rel="icon" href="https://static.thenounproject.com/png/502075-200.png">
 
+    <link href="bootstrap/bootstrap.min.css" rel="stylesheet">
+    <link href="bootstrap/dashboard.css" rel="stylesheet">
+		
+    <script src="bootstrap/jquery-3.2.1.min.js"></script>
+    <script src="bootstrap/popper.min.js"></script>
+    <script src="bootstrap/bootstrap.min.js"></script>
+    <script src="js/ajax.js"></script>
+		
     <!-- Bootstrap core CSS -->
 	<link href="css/mybackgammon.css" rel="stylesheet">
 	<script src="js/mybackgammon.js"></script>
